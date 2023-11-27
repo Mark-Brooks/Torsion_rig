8206f39af11d69771e592b20beae1f27a15f2ffb
Mark-Brooks <88011469+Mark-Brooks@users.noreply.github.com>
2023-11-27 10:32:31 +1000

    Add files via upload
    Modified how to the code receives and processes commands.

--- stepper_v5.ino
+++ stepper_v5.ino
@@ -1,7 +1,7 @@
 // Libraries
 #include <AccelStepper.h>
 
-// Initialise Timings
+// Define Pins
 const int stepPin = 5;
 const int dirPin = 2;
 const int enPin = 8;
@@ -16,59 +16,138 @@ float interval = 500;
 float angle = 0;
 float stepperSpeed;
 
-// Define initial timings
+// Define test case string
+String test_case = "standard";  // Test case, standard by default
+
+// Initialise Timing
 unsigned long previousMillis = 0;
 
 // Displacement
 float displacement = 0;
 
-// Defined Stepper
+// Define Stepper
 AccelStepper stepper(1, stepPin, dirPin);
+
+// Message Types
+#define MSG_TYPE_SET_CONFIG str(SET_CFG)  // sets settings for tests
+#define MSG_TYPE_SET_TEST str(SET_TEST)   // dictates start/stop of tests and test type
 
 void setup() {
   stepper.setMaxSpeed(1000);
   pinMode(enPin, OUTPUT);
   digitalWrite(enPin, LOW);
 
-  Serial.begin(115200);  // Initialize serial communication with a baud rate of 9600.
+  Serial.begin(115200);  // Initialize serial communication with a baud rate of 115200.
+}
+
+void readSerialData() {
+  static String inputData;
+  while (Serial.available() > 0) {
+    char incomingChar = Serial.read();
+    if (incomingChar == '\n' || incomingChar == '\r') {
+      processData(inputData);
+      inputData = "";  // Clear the string for the next line
+    } else {           // Ignore carriage return
+      inputData += incomingChar;
+    }
+  }
+}
+
+void processData(String data) {
+  int firstCommaIndex = data.indexOf(';');
+  String msgType = data.substring(0, firstCommaIndex);
+  String keyValuePairs = data.substring(firstCommaIndex + 1);
+
+  if (msgType == MSG_TYPE_SET_CONFIG) {
+    handleStepperConfig(msgType, keyValuePairs);
+  } else if (msgType == MSG_TYPE_SET_TEST) {
+    handleTest(msgType, keyValuePairs);
+  } else {
+    Serial.println("Unknown Message Type");
+  }
+}
+
+void handleStepperConfig(String msgType, String keyValuePairs) {
+  int semicolonIndex = 0;
+  while ((semicolonIndex = keyValuePairs.indexOf(';')) != -1) {
+    String pair = keyValuePairs.substring(0, semicolonIndex);
+    int commaIndex = pair.indexOf(',');
+
+    if (commaIndex != -1) {
+      String key = pair.substring(0, commaIndex);
+      String value = pair.substring(commaIndex + 1);
+
+      // Using abbreviations for keys
+      if (key == "MS") {  // MS = microSteps
+        micro_steps_float = value.toFloat();
+      } else if (key == "CS") {  // CS = setSpeed
+        rpm_float = value.toFloat();
+        float steps_rev = micro_steps_float * ratio;
+        angle = (360 / (micro_steps_float * ratio));
+        stepperSpeed = steps_rev * rpm_float / 60;
+        stepper.setSpeed(value.toFloat(stepperSpeed));
+      } else {
+        Serial.println("Unknown command: " + key);
+      }
+    } else {
+      Serial.println("Invalid pair format: " + pair);
+    }
+
+    // Update keyValuePairs for the next iteration
+    keyValuePairs = keyValuePairs.substring(semicolonIndex + 1);
+  }
+}
+
+void handleTest(String msgType, String keyValuePairs) {
+  int semicolonIndex = 0;
+  while ((semicolonIndex = keyValuePairs.indexOf(';')) != -1) {
+    String pair = keyValuePairs.substring(0, semicolonIndex);
+    int commaIndex = pair.indexOf(',');
+
+    if (commaIndex != -1) {
+      String key = pair.substring(0, commaIndex);
+      String value = pair.substring(commaIndex + 1);
+
+      // Using abbreviations for keys
+      if (key == "OF") {  // OF = on_off
+        on_off = value;
+      } else if (key == "TC") {  // TC = testCase
+        test_case = value;
+      } else {
+        Serial.println("Unknown command: " + key);
+      }
+    } else {
+      Serial.println("Invalid pair format: " + pair);
+    }
+
+    // Update keyValuePairs for the next iteration
+    keyValuePairs = keyValuePairs.substring(semicolonIndex + 1);
+  }
+}
+
+void standardTest() {
+  // Steps motor at desired speed
+  stepper.runSpeed();
+}
+
+void fatigueTest() {
+  // Add code for fatigue test here
 }
 
 void loop() {
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\r');
+  readSerialData();
 
-    // Find the positions of commas in the input string
-    int comma1 = command.indexOf(',');
-    int comma2 = command.lastIndexOf(',');
-
-    if (comma1 != -1 && comma2 != -1) {
-      // Extract and store the values between commas
-      on_off = command.substring(0, comma1);
-      String micro_steps = command.substring(comma1 + 1, comma2);
-      String rpm = command.substring(comma2 + 1);
-
-      // Convert the extracted strings to integers
-      micro_steps_float = micro_steps.toFloat();
-      rpm_float = rpm.toFloat();
-
-      // Calculate steps per revolution given gear ratio and angle per step
-      float steps_rev = micro_steps_float * ratio;
-      angle = (360 / (micro_steps_float * ratio));
-      stepperSpeed = steps_rev * rpm_float / 60;
-
-      // Stepper Speed
-      stepper.setSpeed(stepperSpeed);
-    }
-  }
-  if (on_off == "0") {
-    displacement = 0;
-  }
   if (on_off == "1") {
     // Time Counting
     unsigned long currentMillis = millis();
 
-    // Step Motor
-    stepper.runSpeed();
+    if (test_case == "standard") {
+      standardTest();
+    } else if (test_case == "fatigue") {
+      fatigueTest();
+    } else {
+      Serial.println("Unknown test type: " + test_case);
+    }
 
     if (currentMillis - previousMillis >= 500) {
       // Recording Position
@@ -76,8 +155,10 @@ void loop() {
       previousMillis = currentMillis;
       Serial.print(displacement);
       Serial.print(",");
-      Serial.print(7);
+      Serial.print(7);  // Placeholder value for torque
       Serial.print("\n");
+    } else {
+      displacement = 0;
     }
   }
 }
